819345ce9b24c7c05c00395e2d9a41602b41deb8
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-07 08:02:54 -0300

    Update restaurante.sql

--- BD/restaurante.sql
+++ BD/restaurante.sql
@@ -1,7 +1,7 @@
 CREATE table restaurante(
 	id_restaurante int not null AUTO_INCREMENT,
   	nome varchar(50) not null, 
-  	id_chefe int not null, /*FK que referencia a tabela funcionario*/
+  	id_chefe int , /*FK que referencia a tabela funcionario*/
   	horario_abertura time,
   	horario_fechar time,
   	cnpj_responsavel char(14) not null,
@@ -53,8 +53,8 @@ CREATE table itemCardapio(
 	id_item int not null AUTO_INCREMENT,
   	id_cardapio int not null, /*FK que referencia a tabela cardapio*/ 
   	valor decimal(5,2),
-  	desconto decimal(2,2),
   	minutos_preparo int,
+  	nome_item varchar(60) null,
   	CONSTRAINT pk_itemCardapio PRIMARY KEY (id_item)
 );
 
@@ -101,4 +101,99 @@ ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE itemCardapio ADD CONSTRAINT fk_itemCardapio_idCardapio
 FOREIGN KEY (id_cardapio) REFERENCES cardapio (id_cardapio)
 ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+/* YYYYMMDD  */
+INSERT INTO licenca_sanitaria (num_licenca, data_emissao, validade, cnpj)
+VALUES ('109', 200310, 210310, '01234567895432'),
+       ('278', 191028, 191028, '25836914701230')
+       ('325', 200310, 210310, '78945612308520'),
+       ('412', 220527, 230527, '12365498774102'),
+       ('569', 210805, 220805, '36985214707896'),
+       ('613', 191003, 201003, '01478520369842'),
+       ('747', 201010, 211010, '25836914701230'),
+       ('856', 210730, 220730, '74125896301258'),
+       ('981', 201228, 211228, '36985214707896'),
+       ('023', 230606, 240606, '98745632102589');
+       
+         
+INSERT INTO federacao (cidade, estado)
+VALUES ('Acre', 'AC'),
+('Alagoas', 'AL'),
+('Amapá', 'AP'),
+('Amazonas', 'AM'),
+('Bahia', 'BA'),
+('Ceará', 'CE'),
+('Espírito Santo', 'ES'),
+('Goiás', 'GO'),
+('Maranhão', 'MA'),
+('Mato Grosso', 'MT'),
+('Mato Grosso do Sul', 'MS'),
+('Minas Gerais', 'MG'),
+('Pará', 'PA'),
+('Paraíba', 'PB'),
+('Paraná', 'PR'),
+('Pernambuco', 'PE'),
+('Piauí', 'PI'),
+('Rio de Janeiro', 'RJ'),
+('Rio Grande do Norte', 'RN'),
+('Rio Grande do Sul', 'RS'),
+('Rondônia', 'RO'),
+('Roraima', 'RR'),
+('Santa Catarina', 'SC'),
+('São Paulo', 'SP'),
+('Sergipe', 'SE'),
+('Tocantins', 'TO');
+       
+INSERT INTO restaurante (nome, id_chefe, horario_abertura, horario_fechar, cnpj_responsavel, alvara_sanitario, categoria, id_fed)
+VALUES 
+('azulee', 3, 190000, 233000,'78945612308520', '325', 'havaiano', 126),
+('Olivetti', 10, 114000, 150000,'12365478995175', '569', 'italiano', 120),
+('Santo Cupim', 18, 200000, 000000, '25896314703574', '613', 'brasileiro', 118),
+('Cantina Gran Sapore', 5 , 110000, 163500, '98745632102589', '023', 'italiano', 133),
+('Montebelo', 12, 114000, 150000,'12365478995175', '856', 'italiano', 122),
+('Romeo & Giulietta', 21, 130000, 210000, '12365478985203', '747', 'doceria', 124),
+('Blossoms', 32, 130000, 210000, '95175368425031', '109', 'doceria', 124),
+('The Tropical Road', 3 , 190000, 213000,'78945612308520', '278', 'havaiano', 119),
+('The Caribbean Balcony', 38, 120000, 153000, '14785236987423','412', 'cubana', 134),
+('Pensão do Baião', 15, 110000, 143000, '95175368425031', '981', 'brasileiro', 136);
+
+INSERT INTO cardapio (id_restaurante)
+VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10)
+	   (1), (2), (3), (4), (5), (6), (7), (8), (9), (10);
+
+INSERT INTO itemCardapio (id_cardapio, nome_item, valor, minutos_preparo)
+VALUES 
+(12, 'risotto de camarão', '69.90', '40'),
+(6, 'pistache brownie', '13.50', '80'),
+(11, 'poke de atum', '58.99', '25'),
+(9, 'Chicharrones', '25.80', '30'),
+(13, 'Arroz Maria Isabel', '47.90', '45'),
+(20, 'Baião de Dois', '58.90', '100'),
+(7, 'Red Velvet cupcacke', '15.00', '90'),
+(4, 'Arancine', '60.30', '20');
+
+INSERT INTO funcionario (id_funcionario, id_restaurante, nome, sobrenome, sexo, data_contratacao, funcao, telefone)
+VALUES
+(null, 1, 'Doe', 'John', 'M', 170214, 'sou-chef', '21-98765432');
+(18, (select id_restaurante from restaurante where id_chefe = 18), 'Jacob', 'Lopez', 'M', 200504, 'chefe', '21-98765432'),
+(12, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'Austin', 'Charles', 'M', 211208, 'chefe', null),
+(5, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'Sheryl', 'Mccarty', 'F', 220714, 'chefe', null),
+(21, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'Andrea', 'Morales', 'M', 180228, 'chefe', null),
+(32, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'John', 'Diaz', 'M', 200805, 'chefe', null),
+(38, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'William','Weber', 'M', 190331, 'chefe', null),
+(15, (select id_restaurante from restaurante where id_chefe = id_funcionario), 'Kristopher', 'Brown', 'M', 220902, 'chefe', null);
+
+
+INSERt INTO fornecedor (id_restaurante, item_fornecido, telefone)
+VALUES 
+(2, 1, 'XX-98765432'),
+(6, 2, 'XX-12365478'),
+(1, 3, 'XX-98745632'),
+(9, 4, 'XX-10258963'),
+(3, 5, 'XX-25896314'),
+(10, 6,'XX-15935785'),
+(7, 7, 'XX-85287469'),
+(4, 8, 'XX-02147852');
+
 
