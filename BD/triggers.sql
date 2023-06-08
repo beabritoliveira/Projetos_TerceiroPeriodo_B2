3360252d714f35ef77b61401fe9720ccafb7a933
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-08 15:42:48 -0300

    Update triggers.sql

--- BD/triggers.sql
+++ BD/triggers.sql
@@ -4,3 +4,11 @@ CREATE TRIGGER povoar_trabalha_em_CHEF AFTER INSERT ON restaurante FOR EACH ROW
     INSERT Into trabalha_em (id_restaurante, id_funcionario)
 	VALUES (new.id_restaurante, new.id_chefe);
 END $$
+
+DELIMITER $$   
+CREATE TRIGGER testando AFTER INSERT ON licenca_sanitaria FOR EACH ROW 
+	BEGIN
+		INSERT INTO conexao (ordem, num_lic)
+		VALUES((select COUNT(num_licenca) from licenca_sanitaria), new.num_licenca);
+        /*(CAST(lic AS CHAR));*/
+END $$
