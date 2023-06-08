6bdf238b126f5713feb5384f8b1370769c7ade51
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-08 19:14:21 -0300

    Update triggers.sql

--- BD/triggers.sql
+++ BD/triggers.sql
@@ -13,10 +13,3 @@ CREATE TRIGGER povoar_trabalha_em_CHEF AFTER INSERT ON restaurante FOR EACH ROW
 	VALUES (new.id_restaurante, new.id_chefe);
 END $$
 
-
-DELIMITER $$   
-CREATE TRIGGER cadastrar_cardapio AFTER INSERT ON restaurante FOR EACH ROW 
-	BEGIN
-    INSERT INTO trabalha_em (id_restaurante)
-	VALUES (new.id_restaurante);
-END $$
